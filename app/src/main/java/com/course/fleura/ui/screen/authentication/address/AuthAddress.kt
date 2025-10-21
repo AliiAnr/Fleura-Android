4e5a1ee21d028df10116c23e7afe8329dfa7f14d
Ali An Nuur <alianur043@gmail.com>
2025-10-21 23:53:04 +0700

    add: Auth Address

---
+++ app/src/main/java/com/course/fleura/ui/screen/authentication/address/AuthAddress.kt
@@ -0,0 +1,9 @@
+package com.course.fleura.ui.screen.authentication.address
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AuthAddress(modifier: Modifier = Modifier) {
+    
+}
